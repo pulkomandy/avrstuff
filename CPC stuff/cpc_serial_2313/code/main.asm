741c0b9ad7eedf8baed139bf442391078ed4fa0e
Adrien Destugues <pulkomandy@pulkomandy.tk>
2010-10-29 17:04:18 +0000

    Some optimizations to get faster interrupts Some comments in main to tell what it should be doing
    git-svn-id: svn://pulkomandy.tk/avrstuff@28 c6672c3c-f6b6-47f9-9001-1fd6b12fecbe

--- CPC stuff/cpc_serial_2313/code/main.asm
+++ CPC stuff/cpc_serial_2313/code/main.asm
@@ -17,6 +17,10 @@
 
 .EQU curregbak = SRAM_START
 
+; REGISTERS ALLOCATION
+; R0 = 255 used in interrupt handler for fast switching of DATADIR
+; X (R27 & R26) used in interrupt for fast addressing of regs
+
 .CSEG
 ; Vectors
 ; reset
@@ -52,12 +56,11 @@ cpc_read:
 
 ; We assume X (R26:R27) points to the current reg
 ; So we can load it and react fast enough to the interrupt
-	LDI R16,ALL_OUT ; 1 ; peut être économisé si on sacrifie un reg
-	OUT DATADIR,R0 ; 1
+	OUT DATADIR,R16 	; 1
 	LD R27,X			; 2 cycles ; peut être économisé si un reg. contient
 									; déjà la valeur à envoyer
 									; (mais qui l'update ?)
-	OUT DATAOUT, R27		; 1 cycle
+	OUT DATAOUT, R27	; 1 cycle
 
 ; Here data is sent, the CPC read operation is handled.
 ; We now wait for the end of the read cycle.
@@ -67,12 +70,12 @@ cpc_read:
 ; it is much more relaxed, as we have 12 CPC cycles = 60 AVR cycles free.
 
 ; Restore R27
-	LDI R27,curregbak
-	LD R27,X
+	LDS R27,curregbak
 
 ; release the bus
 	LDI R16,ALL_IN
 	OUT DATADIR, R16
+	LDI R16,ALL_OUT
 
 ; Restore R27 to selected reg. (we erased it to do the OUT)
 	RETI
@@ -81,10 +84,10 @@ cpc_read:
 ; --- WRITE INTERRUPT ---
 cpc_write:
 ; The timing is a bit less constraining here.
-	PUSH R0
-	IN R0,DATAIN
+	PUSH R0						; 2 cycles
+	IN R0,DATAIN				; 1
 ; we also need to know A0 state...
-	SBIS CTRLIN,A0
+	SBIS CTRLIN,A0				; 1
 		; This was actually a reg select operation!
 		; Jump to the proper code
 	RJMP regSel
@@ -95,8 +98,7 @@ cpc_write:
 	RJMP intEnd
 
 regSel:
-	LDI R27,curregbak
-	ST X,R0
+	STS curregbak,R0
 	MOV R27,R0
 
 intEnd:
@@ -109,4 +111,15 @@ intEnd:
 ; At a bare minimum :
 ; * Set up the INT0 and INT1 so the CPC can do the rest of the setup itself
 init:
-	RJMP init
+	; setup ctrl port : RW and A0 as inputs, INT as output
+	; led on (will be turned off by software at init)
+	; init serial port speed and io
+	; check for bootloader jumper and jump to bootload code if needed
+
+	SEI
+mainloop:
+	; maybe we will have to handle a buffer for the serial port
+	; and 'fake' registers in SRAM
+
+	SLEEP
+	RJMP mainloop
