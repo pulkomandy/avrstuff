3c1705764a8941b0f57df66acb9843924c069462
Adrien Destugues <pulkomandy@pulkomandy.tk>
2012-01-15 09:44:43 +0000

    Fix bugs related to minimal-size blocks. Loading a standard game works !
    git-svn-id: svn://pulkomandy.tk/avrstuff@54 c6672c3c-f6b6-47f9-9001-1fd6b12fecbe

--- V-USB_Dev/firmwares/CrO2/main.c
+++ V-USB_Dev/firmwares/CrO2/main.c
@@ -13,6 +13,7 @@ static uint8_t ioblock[275]; // Max. Size of an MO5 standard block including syn
 static uint8_t status; // status bits
 static uint16_t writePos;
 static uint16_t blksz;
+static uint8_t cksum;
 
 static uint16_t readPos;
 static uint8_t bitpos;
@@ -40,32 +41,8 @@ int main() {
 	{
 		ioblock[i] = 1;
 	}
-	ioblock[i++] = 0x3C;
-	ioblock[i++] = 0x5A;
-
-	// HEAD block
-	ioblock[i++] = 0x0;
-	ioblock[i++] = 0x10;
-
-	int j = i;
-	ioblock[i++] = 'C';
-	ioblock[i++] = 'r';
-	ioblock[i++] = 'O';
-	ioblock[i++] = '2';
-	ioblock[i++] = ' ';
-	ioblock[i++] = ' ';
-	ioblock[i++] = ' ';
-	ioblock[i++] = ' ';
-	ioblock[i++] = 'B';
-	ioblock[i++] = 'A';
-	ioblock[i++] = 'S';
-	ioblock[i++] = 0;
-	ioblock[i++] = 0;
-	ioblock[i++] = 0;
-	uint8_t s = 0;
-	for (;j<i;j++) s -= ioblock[j];
-	ioblock[i++] = s;
-
+	ioblock[16] = 0x3C;
+	ioblock[17] = 0x5A;
 
 	// Setup TIMER 1 for MFM pulses generation
 	TCCR1A = (1<<COM1A0); // Enable OC1A output
@@ -88,6 +65,13 @@ int main() {
 }
 
 
+static inline void startGen()
+{
+	TCCR1A &= ~(1<<COM1A1);	// Toggle output on compare match
+	TCCR1B |= (1<<CS10);	// Enable timer clock (prescaler = 1)
+}
+
+
 uint8_t usbFunctionSetup(uint8_t data[8]) {
 	usbRequest_t *rq = (void *)data;
 
@@ -107,10 +91,21 @@ uint8_t usbFunctionSetup(uint8_t data[8]) {
 			{
 				/* SEND DATA (USB > TAPE) */
 				/* Get ready, actual data will come in usbFunctionWrite */
-				// TODO store type of MO5 block + size in ioblock
-				writePos = 0;
-				blksz = rq->wLength.word;
-				return USB_NO_MSG; // Call usbFunctionWrite to send the data
+
+				// store type of MO5 block + size in ioblock
+				writePos = 20; // Leave space for Sync header, block type, and block size.
+				ioblock[18] = rq->wValue.word;
+				ioblock[19] = rq->wLength.word + 2; // Add blktype and cksum to data length
+				blksz = rq->wLength.word + 21;
+				cksum = 0;
+				if (rq->wLength.word == 0)
+				{
+					// No data in this block - start generating now !
+					ioblock[20] = 0; // checksum
+					startGen();
+				} else {
+					return USB_NO_MSG; // Call usbFunctionWrite to send the data
+				}
 			}
 	}
 	return 0;
@@ -121,19 +116,17 @@ uint8_t usbFunctionWrite(uint8_t* data, uint8_t len)
 	// The data for a single request will be sent as slices of up to 8 bytes.
 	for (int i = 0; i < len; i++)
 	{
-		// TODO leave place for sync header and block type+size
-//		ioblock[writePos] = data[i];
+		ioblock[writePos] = data[i];
+		cksum -= data[i];
 		writePos++;
 	}
 
-	// TODO compute checksum as we go
+	ioblock[writePos] = cksum;
 
-	if (writePos >= blksz)
+	if (writePos >= blksz - 1)
 	{
 		// start generating
-		TCCR1A &= ~(1<<COM1A1);
-		TCCR1B |= (1<<CS10);
-		blksz = 35;
+		startGen();
 		return 1;
 	} else {
 		return 0;
@@ -154,11 +147,14 @@ ISR (TIMER1_COMPA_vect, ISR_NOBLOCK)
 		readPos++;
 		if (readPos > blksz)
 		{
-			// Make sure output is high
+			// Make sure output is high step 1: match action = force high level
 			TCCR1A |= (1<<COM1A1);
+
 			// Stop generating (and interrupts)
 			TCCR1B &= ~(1<<CS10);
 			readPos = 0;
+
+			// make sure output is high step 2 : force a match.
 			TCCR1A |= (1 << FOC1A);
 		}
 	}
